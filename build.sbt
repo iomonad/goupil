69829a56fd1df79b83032a41ca866fef444851bf
Clement Trosa <me@trosa.io>
2018-06-12 11:11:25 +0200

    Added few actors

--- build.sbt
+++ build.sbt
@@ -1,18 +1,23 @@
 import Dependencies._
 
+resolvers += "jitpack" at "https://jitpack.io"
+
 /* Global configuration */
 lazy val root = (project in file(".")).
-  settings(
-    inThisBuild(List(
-      organization := "io.trosa",
-      scalaVersion := "2.12.2",
-      version := "0.0.1"
-    )),
-    name := "goupil",
-    libraryDependencies ++= Seq(
-      scalaTest % Test
-    )
-  )
+	settings(
+		inThisBuild(List(
+			organization := "io.trosa",
+			scalaVersion := "2.12.2",
+			version := "0.0.1"
+		)),
+		name := "goupil",
+		libraryDependencies ++= Seq(
+			scalaTest % Test,
+			cats,
+			akka,
+			slack
+		)
+	)
 
 /* Boot endpoint */
 mainClass in Compile := Some("io.trosa.goupil")
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,16 +2,21 @@ import sbt._
 
 object Dependencies {
 
-  /*
-  * Versions
-  * */
+  	/*
+  	* Versions
+  	* */
 
-  lazy val testVersion = "3.0.1"
+	lazy val testVersion = "3.0.1"
+  	lazy val catsVersion = "1.0.1"
+	lazy val akkaVersion = "2.5.13"
+	lazy val slackVersion = "1.2.0"
 
+  	/*
+  	* Dependencies
+  	* */
 
-  /*
-  * Dependencies
-  * */
-
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % testVersion
+  	lazy val scalaTest = "org.scalatest" %% "scalatest" % testVersion
+  	lazy val cats = "org.typelevel" %% "cats-core" % catsVersion
+	lazy val akka = "com.typesafe.akka" %% "akka-actor" % akkaVersion
+	lazy val slack = "com.github.Ullink" % "simple-slack-api" % slackVersion
 }
